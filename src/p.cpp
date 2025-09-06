504b6b29ea14894a0d43f9f7963ec193de9043b6
AmericoCh <americochulluncuy@gmail.com>
2025-09-06 11:48:48 -0500

    dirigidas

---
+++ src/p.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+using namespace std;
+
+int busquedaBinaria(int arr[], int n, int valor) {
+    int inicio = 0;
+    int fin = n-1;
+    int medio = 0;
+    while(inicio < fin) {
+        medio = (inicio + fin)/2;
+        if(arr[medio] == valor) {
+            return medio;
+        } else if(arr[medio] < valor) {
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+    return -1;
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int* L = new int[n1];
+    int* R = new int[n2];
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i]; i++;
+        } else {
+            arr[k] = R[j]; j++;
+        }
+        k++;
+    }
+
+    while (i < n1) { arr[k] = L[i]; i++; k++; }
+    while (j < n2) { arr[k] = R[j]; j++; k++; }
+
+    delete[] L;
+    delete[] R;
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; ++j) {
+            if (arr[j] < pivot) {
+                i++;
+                std::swap(arr[i], arr[j]);
+            }
+        }
+        std::swap(arr[i + 1], arr[high]);
+        int pi = i + 1;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int low = 0, high = n - 1;
+while (low <= high) {
+    int mid = (low + high) / 2;
+    if (arr[mid] == key) {
+        std::cout << "Encontrado en: " << mid << std::endl;
+        break;
+    } else if (arr[mid] < key) {
+        low = mid + 1;
+    } else {
+        high = mid - 1;
+    }
+}
+
+int main(){
+
+    return 0;
+}
+
+
