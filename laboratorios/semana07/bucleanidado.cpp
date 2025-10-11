eaa48845c2c380b80c9e0f52324855dc132c1564
AmericoCh <americochulluncuy@gmail.com>
2025-10-11 13:49:54 -0500

    semana7

---
+++ laboratorios/semana07/bucleanidado.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+
+    for (char filas = 'Z'; filas >= 'A'; --filas) {
+        for (char columnas = filas; columnas >= 'A'; --columnas) {
+            cout << columnas;
+        }
+        cout << endl;
+        
+    }
+
+    return 0;
+}
--- laboratorios/semana07/ejemplo02.cpp
+++ laboratorios/semana07/ejemplo02.cpp
@@ -20,40 +20,43 @@ Al finalizar, preguntar (con do-while) si desea generar otro tablero.
 using namespace std;
 
 int main(){
+
     int n;
     char respuesta;
+    
     do {
-        
+        // Validar n
         do {
-            cout << "Ingrese el tamano del tablero (3 - 15): ";
+             cout << "ingrese el tamano del tablero (emtre 3 y 15): ";
             cin >> n;
             if (n < 3) {
-                cout << "Error, el tamano es menor que 3" <<endl;
-                cout << "Intente nuevamente" <<endl;
-            } else if (n > 15){
-                cout << "El tamano es mayor que 15" <<endl;
-                cout << "Intente nuevamente" <<endl;
-            }
+                cout << "Error, el valor es menor que 3"<<endl;
+                cout << "Vuelva a ingresar el valor de n"<<endl;
+            } else if ( n > 15) {
+                cout << "Error, el valor es mayor que 3"<<endl;
+                cout << "Vuelva a ingresar el valor de n"<<endl;
+            } 
         } while (n < 3 || n > 15);
 
         cout << "TABLERO GENERADO" <<endl;
-        for (int filas = 1; filas <= n; ++filas) { //para recorrer filas
-            for (int cols = 1; cols <= n; ++cols) {
-                if (filas % 3 == 0 || cols % 3 == 0) {
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                //Si la suma de los índices (i + j) es par, colocar el carácter 'X'.
+                if (i % 3 == 0 || j % 3 == 0) {
                     cout << "# ";
-                } else if ((filas + cols) % 2 == 0 ) {
+                } else if ((i + j) % 2 == 0) {
                     cout << "X ";
                 } else {
-                    cout << "0 ";
+                    cout << "O ";
                 }
             }
             cout << endl;
         }
 
-    cout << "Desea continuar? (s/n)" <<endl;
-    cin >> respuesta;
-
+        cout << "¿Desea continuar?" <<endl;
+        cin >> respuesta;
     } while (respuesta == 's');
-
+ 
     return 0;
 }
