bb8ec5ec72df755ec3276d44529730659c1f7a00
AmericoCh <americochulluncuy@gmail.com>
2025-10-23 07:43:49 -0500

    semana09

--- laboratorios/semana09/arreglos.cpp
+++ laboratorios/semana09/arreglos.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main(){
+    // declaracion de un arreglo
+    int notas[15] = {14, 8, 9};
+    int ceros[10] = {0}; // inicializando todos los elementos con 0
+
+    //Inicializando elementos 
+    // notas[0] = 14;
+    // notas[1] = 8;
+    // notas[2] = 9;
+
+    //Imprimiendo los elementos del arreglo
+    cout << notas[0] <<endl;
+    for (int i = 0; i < 15; ++i) {
+        cout << notas[i] << " ";
+    }
+
+    // Solicitando datos al usuario
+    cout << "Ingrese lo elementos del arreglo: " <<endl;
+    for (int i = 0; i < 15; ++i) {
+        cin >>notas[i];
+    }
 
     return 0;
 }
--- laboratorios/semana09/burbuja.cpp
+++ laboratorios/semana09/burbuja.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 
 int main(){
+    //Algoritmo burbuja
+    //int a[]; //ERROR
+    int arreglo[] = {17,23,5,11};
+
+    int n = sizeof(arreglo)/sizeof(arreglo[0]);
+
+    //Arreglo original
+    cout << "Arreglo original: " <<endl;
+    for (int i = 0; i < n; ++i) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl;
+
+    
+    for (int i = 0; i < n - 1; ++i) { //numero pasadas
+        for (int j = 0; j < n - 1  - i; ++j) { // comparaciones
+            if (arreglo[j] > arreglo[j+1]) { // intercambiar
+                int temp = arreglo[j];
+                arreglo[j] = arreglo[j+1];
+                arreglo[j+1] = temp;
+            }
+        }
+    }
+
+    //Arreglo ordenado
+    cout << "Arreglo ordenado en forma creciente: " <<endl;
+    for (int i = 0; i < n; ++i) {
+        cout << arreglo[i] << " ";
+    }
+
+
+    
 
     return 0;
 }
