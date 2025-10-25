4b4ab70d0e03c26b267c68667535e09138524333
AmericoCh <americochulluncuy@gmail.com>
2025-10-25 14:06:59 -0500

    Dirigida9, arreglos

--- laboratorios/semana09/arreglos.cpp
+++ laboratorios/semana09/arreglos.cpp
@@ -1,27 +1,61 @@
 ﻿#include <iostream>
 using namespace std;
 
+const int TAM1 = 30;
+
+
 int main(){
-    // declaracion de un arreglo
-    int notas[15] = {14, 8, 9};
-    int ceros[10] = {0}; // inicializando todos los elementos con 0
 
-    //Inicializando elementos 
-    // notas[0] = 14;
-    // notas[1] = 8;
-    // notas[2] = 9;
+    // Declaracion de un arreglo
+    float temperaturas[20];
 
-    //Imprimiendo los elementos del arreglo
-    cout << notas[0] <<endl;
-    for (int i = 0; i < 15; ++i) {
+    // los índices para acceder a los elementos de una arreglo 
+    // comienzan en 0
+    // cout << temperaturas[0] <<endl;  // puede contener cualquier dato
+
+    // Declarando e inicializando un arreglo
+    int notas[5] = {11,9,13,17,5};
+    
+    // imprimir el arreglo
+    for(int i = 0; i < 5; ++i) {
         cout << notas[i] << " ";
     }
+    cout<< endl;
 
-    // Solicitando datos al usuario
-    cout << "Ingrese lo elementos del arreglo: " <<endl;
-    for (int i = 0; i < 15; ++i) {
-        cin >>notas[i];
+    int notasParcial[5];
+    cout << "Ingrese las 5 notas del Parcial:\n ";
+
+    for (int i = 0; i < 5; ++i) {
+        cout << "Ingrese la nota  " << i +1 << ": ";
+        cin >> notasParcial[i];
     }
+
+
+    int notasFinal[] = {13,15,16,11,10,13,12,13,5,7};
+
+    //recuperando el tamaño de notasFinal
+    int n = sizeof(notasFinal)/sizeof(notasFinal[0]);
+
+    for (int i = 0 ; i < n; ++i) {
+        cout << notasFinal[i]  << " ";
+    }
+    cout << endl;
+
+    // ERROR 
+    int m;
+    cout << "Ingrese el tamaño del arreglo";
+    cin >> m;
+    int indefinido[m];
+
+    // FORMA CORRECTA
+    const int TAM = 6;
+    int correcto1[TAM];
+
+    int correcto2[TAM1];
+
+    int arr[10] = {1,2,3}; // el compilador completa los demas elementos con ceros
+
+    int arr1[20] = {0}; // inificializa todos los elementos del arreglo en cero
 
     return 0;
 }
--- laboratorios/semana09/burbuja.cpp
+++ laboratorios/semana09/burbuja.cpp
@@ -2,38 +2,35 @@
 using namespace std;
 
 int main(){
-    //Algoritmo burbuja
-    //int a[]; //ERROR
+
     int arreglo[] = {17,23,5,11};
 
     int n = sizeof(arreglo)/sizeof(arreglo[0]);
 
-    //Arreglo original
-    cout << "Arreglo original: " <<endl;
-    for (int i = 0; i < n; ++i) {
+    cout << "Arreglo original: \n";
+    for (int i = 0 ; i < n; ++i) {
         cout << arreglo[i] << " ";
     }
     cout << endl;
 
-    
-    for (int i = 0; i < n - 1; ++i) { //numero pasadas
-        for (int j = 0; j < n - 1  - i; ++j) { // comparaciones
-            if (arreglo[j] > arreglo[j+1]) { // intercambiar
+    //Algoritmo Burbuja (bubbleSort)
+    for(int i = 0; i < n - 1; ++i) { //pasadas en el arreglo
+        for (int j = 0; j < n - 1 - i; ++j) { // comparaciones
+            if(arreglo[j] > arreglo[j+1]) {
                 int temp = arreglo[j];
                 arreglo[j] = arreglo[j+1];
                 arreglo[j+1] = temp;
             }
         }
+
     }
 
-    //Arreglo ordenado
-    cout << "Arreglo ordenado en forma creciente: " <<endl;
-    for (int i = 0; i < n; ++i) {
+    cout << "Arreglo ordenado en forma creciente: \n";
+    for (int i = 0 ; i < n; ++i) {
         cout << arreglo[i] << " ";
     }
+    cout << endl;
 
-
-    
 
     return 0;
 }
