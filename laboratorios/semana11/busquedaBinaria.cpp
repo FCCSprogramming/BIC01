9817fc141070d7c43ec4649ac0be326916b000f3
AmericoCh <americochulluncuy@gmail.com>
2025-11-06 07:47:16 -0500

    ejemplos11

---
+++ laboratorios/semana11/busquedaBinaria.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int arr[] = {1, 2, 4, 7, 8, 9, 11, 17, 19, 23, 30, 34};
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int clave = 23;
+    int indice  = -1;
+
+    int inicio = 0;
+    int fin = n-1;   // (inicio + fin) / 2   <> calcula el cociente entero
+    int medio;
+
+    // Búsqueda Binaria
+
+    while(inicio <= fin) {
+        medio = (inicio + fin) / 2;
+        if (arr[medio] == clave) {
+            indice = medio;
+            break;
+        } else if(arr[medio] < clave) {
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+
+    
+
+
+
+    
+    return 0;
+}
---
+++ laboratorios/semana11/busquedaLineal.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {2,6,3,8,4,9,1,9,3,0,1,-3};
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Búsqueda lineal
+    int clave = 11;
+    int indice = -1;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == clave) {
+            indice = i;
+            break;
+        }
+    }
+
+    if (indice == -1) {
+        cout << "El elemento " << clave << " no se encuentra en el arreglo\n";
+    } else {
+        cout << "El elemento " << clave << " se encuentra en la posición " << indice << endl;
+    }
+
+    return 0;
+}
