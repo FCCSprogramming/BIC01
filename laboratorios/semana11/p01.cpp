cf195e05f703110307721fe516f2ab19c38cf1c8
AmericoCh <americochulluncuy@gmail.com>
2025-11-08 14:57:29 -0500

    ejercicios 1, 2

---
+++ laboratorios/semana11/p01.cpp
@@ -0,0 +1,61 @@
+﻿/*
+1. Escribir un programa que, al recibir como dato un arreglo bidimensional cuadrado de
+tipo entero y dimensión 10, imprima la diagonal de dicho arreglo y a continuación
+ordene los elementos de dicha diagonal en forma ascendente
+*/
+
+#include <iostream>
+#include <iomanip> //setw()
+#include <cstdlib> // para rand() y srand()
+#include <ctime> // para time()
+using namespace std;
+
+int main(){
+
+    // Declaramos una matriz de 10 x 10
+    int  matriz[10][10];
+
+    // Generamos elementos de forma aleatoria
+    srand(time(0)); // actualizamos la semilla en función del tiempo de su PC
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            matriz[i][j] = rand() % 10 + 1;
+        }
+    }
+
+    // Mostrando la matriz generada
+    cout << "\nLa matriz genera es\n";
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            cout << setw(2) << matriz[i][j] << "  ";
+        }
+        cout << endl;
+    }
+
+    // imprimir la Diagonal de la matriz
+    cout << "\n\nLa diagonal de ma matriz es:\n ";
+    for (int i = 0; i < 10; ++i) {
+        cout << matriz[i][i] << " ";
+    }
+    cout << "\n\n";
+    
+    // Ordenando los elemento de la diagonal en forma creciente
+    for (int i = 0; i < 10 - 1; ++i) { // recorridos
+        for (int j = 0; j < 10 - i - 1; ++j) {
+            if (matriz[j][j] > matriz[j+1][j+1]) {
+                swap(matriz[j][j], matriz[j+1][j+1]);
+            }
+        }
+    }
+
+    // imprimimos la matriz con la diagonal ordenada
+    cout << "\nMatriz con la diagonal ordenada\n";
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            cout << setw(2) << matriz[i][j] << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ laboratorios/semana11/p02.cpp
@@ -0,0 +1,43 @@
+﻿/*
+2. Escribir un programa que calcule los valores máximo y mínimo
+y sus respectivas posiciones dentro de una matriz dada por el usuario
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int filas = 2;
+    const int columnas = 3;
+
+    int matriz[filas][columnas] = {{1,6,-8},
+                        {3,5,0}};
+
+    int maxMatriz = matriz[0][0]; // asumimos un valor inicial
+    int minMatriz = matriz[0][0];
+
+    int posFilaMax = 0, posColMax = 0;
+    int posFilaMin = 0, posColMin = 0;
+
+    //  recorremos la matriz y actualizar las variables maxMatriz, minMatriz,
+    for (int i = 0; i < filas; ++i) { // recorremos filas
+        for (int j = 0; j < columnas; ++j) { // recorremos columnas
+            // Actualizar maxMatriz
+            if (matriz[i][j] > maxMatriz) {
+                maxMatriz = matriz[i][j];
+                posFilaMax = i;
+                posColMax = j;
+            }
+
+            // Actualizar minMatriz
+            if (matriz[i][j] < minMatriz) {
+                minMatriz = matriz[i][j];
+                posFilaMin = i;
+                posColMin = j;
+            }
+        }
+    }
+    
+
+    return 0;
+}
