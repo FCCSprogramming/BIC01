7c4a0b1c1504f4d622f6c0290f8d9861608727f6
AmericoCh <americochulluncuy@gmail.com>
2025-11-22 16:01:28 -0500

    ejercicios dirigida 13

---
+++ laboratorios/semana13/ejercicios/p09.cpp
@@ -0,0 +1,23 @@
+﻿/*
+TAREA ejercicio 09
+Escribir una función para calcular las coordenadas x e y
+de la trayectoria de un proyectil de acuerdo a los parámetros
+ángulo de inclinación α = 30° y velocidad inicial v a intervalos
+de 0,1 s.
+*/
+
+#include <iostream>
+#include <iomanip> //setw(4)
+#include <cmath> // sin(), cos()  trabajan con argumentos en radianes
+using namespace std;
+
+const double PI = 3.141592;
+const double G = 9.81;
+
+
+
+int main(){
+
+    return 0;
+}
+
